27aa181ef94486d3838aa08d4a5d3aea7d066e94
Jamie Zeng <50186812+jz73243n@users.noreply.github.com>
2022-04-26 19:30:57 -0400

    Add files via upload

---
+++ app/website/special_reports/reduce_criminal_footprint.R
@@ -0,0 +1,9 @@
+reduceCriminalFootprintPanel <- tabPanel(
+  title = a(
+    "Reducing the Criminal Justice Footprint in Manhattan", 
+    target = "_blank", 
+    # located in www/special_reports folder
+    href = "special_reports/Reducing_Criminal_Justice_Footprint_Manhattan.pdf"
+  ), 
+  value = "tabpanel_criminal_footprint",
+)
---
+++ app/website/special_reports/sentencing_alternatives.R
@@ -0,0 +1,9 @@
+sentencingAlternativesPanel <- tabPanel(
+  title = a(
+    "Sentencing Alternatives in Manhattan", 
+    target = "_blank", 
+    # located in www/special_reports folder
+    href = "special_reports/Sentencing_Alternatives_In_Manhattan.pdf"
+  ), 
+  value = "tabpanel_sentencing_alternatives",
+)
